778f2dec6784f3d2ccec080bfa4e7909e720e1be
Mattheo4427 <matdas4427@gmail.com>
2025-03-12 12:32:50 +0100

    Initial Commit

---
+++ app-ios-awi/app_ios_awiApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_ios_awiApp.swift
+//  app-ios-awi
+//
+//  Created by etud on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct app_ios_awiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
